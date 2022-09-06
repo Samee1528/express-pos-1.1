1f56aa0f4ca9ce1e17dfb57f8b48e504d7daee7b
samee1528 <dineshkosala1528@gmail.com>
2022-09-07 02:46:54 +0530

    Content Update

--- app.js
+++ app.js
@@ -2,6 +2,27 @@ const express = require('express')
 const app = express()
 const port = 4001
 
+app.use(express.json ())
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.post('/', (req, res) => {
+    res.send('Request send in post')
+})
+
+app.get('/customer', (req, res) => {
+    res.send('Get Request')
+})
+
+app.post('/customer', (req, res)=> {
+    console.log(req.body.id);
+    res.send('<h2>Post Request  Customer</h2>')
+})
+  
+  
+
 app.listen(port, () => {
     console.log(`app listening on port ${port}`);
 })
